e0a632bd573a9f19a77c847fd01e6a62a1f81cf0
Faber Bernardo Junior <fa056564@intelbras.com.br>
2021-11-23 13:48:21 -0300

    code format

--- 23-11-2.c
+++ 23-11-2.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
-    int i, N; //variaveis
+int main() {
+  int i, N;  // variaveis
 
-    printf("Entre com o número\n");//printa na tela 
-    scanf ("%d", &N);//verifica o que foi digitado
-    while (N<=0){//evita numeros negativos
-        printf("Entre com o número positivo\n");
-        scanf ("%d", &N);
-    }
-    for (i=N;i>0;i--) {//variável que define que o valor vai ser verificado baseado no N de entrada
-        if (i%2==0)//calculo de divisão, se o número for quebrado, ele é primo
-            printf("não é primo ====> %d\n", i);
-        else 
-            printf("número é primo ====> %d\n", i); 
-    }  
-    printf("FIM\n");//finaliza código
+  printf("Entre com o número\n");
+  scanf("%d", &N);
+  while (N <= 0) {
+    printf("Entre com o número positivo\n");
+    scanf("%d", &N);
+  }
+  for (i = N; i > 0; i--) {
+    if (i % 2 == 0)
+      printf("não é primo ====> %d\n", i);
+    else
+      printf("número é primo ====> %d\n", i);
+  }
+  printf("FIM\n");  // finaliza código
 
-    return 0;
+  return 0;
 }
--- 23-11.c
+++ 23-11.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
-int main(){
+int main() {
   float imputN, nCount, fatN, mCount, imputM, fatM, out;
 
   printf("Entre com o número N\n");
-  scanf ("%f", &nCount);
+  scanf("%f", &nCount);
   printf("Entre com o número M\n");
-  scanf ("%f", &mCount);
+  scanf("%f", &mCount);
 
-  imputN=nCount;
-  fatN=1;
-  imputM=mCount;
-  fatM=1;
+  imputN = nCount;
+  fatN = 1;
+  imputM = mCount;
+  fatM = 1;
 
-  while (imputN>1){
-    fatN=fatN*imputN;
-    imputN=imputN-1;	
+  while (imputN > 1) {
+    fatN = fatN * imputN;
+    imputN = imputN - 1;
   }
-  while (imputM>1){
-    fatM=fatM*imputM;
-    imputM=imputM-1;
+  while (imputM > 1) {
+    fatM = fatM * imputM;
+    imputM = imputM - 1;
   }
-  out= (fatN*2)/(fatM+1);
-  
-  printf("Valor de saida é %f, o valor de N é %.1f e o valor de M é %.1f\n", out, fatN, fatM);
+  out = (fatN * 2) / (fatM + 1);
+
+  printf("Valor de saida é %f, o valor de N é %.1f e o valor de M é %.1f\n",
+         out, fatN, fatM);
   return 0;
 }
