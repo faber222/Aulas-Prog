71af7a22edc1da757718123902f2466051ca907d
faber222 <faberbernardo78@gmail.com>
2022-02-10 18:54:42 -0300

    Learning C

--- MT.2.3.c
+++ MT.2.3.c
@@ -7,7 +7,7 @@
 /**********************************/
 
 #define TAM_MAT 10
-#define MAX_TIROS 30
+#define MAX_TIROS 15
 #define MAX_NOME 30
 
 #define LIVRE 0
@@ -21,7 +21,7 @@
 
 int ZonaDeGuerra[TAM_MAT][TAM_MAT]; /* Matriz do espaço de batalha */
 int ContadorTiros = 0;
-int PontuacaoFinal; /* acumula a pontuação do jogador */
+int PontuacaoFinal = 0; /* acumula a pontuação do jogador */
 char nome[MAX_NOME];
 int x_sorteado;
 int y_sorteado;
@@ -118,18 +118,52 @@ void ImprimeMatrizEscondida() {
   for (i = 0; i < TAM_MAT; i++) ImprimeLinhaEscondida(i);
 }
 
-int Tiro() { /* implementar a função Tiro conforme enunciado acima */ }
+int Tiro() {
+  int i, j;
+  int posicaoTiro[TAM_MAT][TAM_MAT];
+  // add posi~ção while para evitar numeros > 9 e < 1
+
+  printf("\nEntre com a posicao de x\n");
+  scanf("%d", &i);
+  printf("\nEntre com a posicao de y\n");
+  scanf("%d", &j);
+
+  if (posicaoTiro[i][j] == LIVRE) {
+    printf("\nVocê errou, tente novamente\n");
+  } else if (posicaoTiro[i][j] == FRAGATA) {
+    printf("\nVocê acertou uma Fragata\n");
+    PontuacaoFinal = PontuacaoFinal + 3;
+  } else if (posicaoTiro[i][j] == SUBMARINO) {
+    printf("\nVocê acertou um Submarino\n");
+    PontuacaoFinal = PontuacaoFinal + 5;
+  } else if (posicaoTiro[i][j] == PORTA_AVIOES) {
+    printf("\nVocê acertou um Porta Avioes\n");
+    PontuacaoFinal = PontuacaoFinal + 10;
+  }
+  ContadorTiros++;
+
+  if (ContadorTiros == MAX_TIROS) {
+    return 2;
+  } else if (PontuacaoFinal == 50) {
+    return 1;
+  }
+  return 0;
+}
 
 int main() {
   int situacao;
   ZerarMatriz();
   PosicionarNavios();
-  ImprimeMatrizEscondida();
+  ImprimeMatrizTudo();
   do {
     situacao = Tiro();
   } while (situacao == 0);
-  /*
-    Implementar aqui o tratamento final da variável situação
-  */
+  if (situacao == 2) {
+    printf("\nSeus tiros acabaram e sua pontuacao final foi de %d\n",
+           PontuacaoFinal);
+  } else if (situacao == 1) {
+    printf("\nVoce destruiu todos os navios %d\n", PontuacaoFinal);
+  }
+
   return 0;
 }
