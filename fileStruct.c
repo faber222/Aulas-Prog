980145f934eeaf9d6bf370de213414f7ce9fb431
faber222 <faberbernardo78@gmail.com>
2022-02-17 18:56:08 -0300

    Learning C

--- fileStruct.c
+++ fileStruct.c
@@ -5,9 +5,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct horario {
+  int horas;
+  int minutos;
+  int segundos;
+  float teste;
+  char palavra;
+};
+struct horario teste(struct horario x) {
+  printf("%d:%d:%d\n", x.horas, x.minutos, x.segundos);
+  x.horas = 100;
+  x.minutos = 100;
+  x.segundos = 100;
+  return x;
+};
+
 int main(void) {
-  struct compras {
-    char item[100];
-    int amount;
-  } carrinho;
+  // struct horario teste(struct horario x);
+
+  struct horario agora;
+  agora.horas = 17;
+  agora.minutos = 18;
+  agora.segundos = 30;
+  struct horario proxima;
+  proxima = teste(agora);
+
+  printf("%i:%i:%i\n", proxima.horas, proxima.minutos, proxima.segundos);
+
+  system("pause");
+  return 0;
 }
