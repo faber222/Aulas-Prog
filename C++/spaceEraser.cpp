842cd00918f0b204f71b9694b5b03c260dde0632
faber222 <faberbernardo78@gmail.com>
2022-04-05 13:34:20 -0300

    Learning C

--- C++/spaceEraser.cpp
+++ C++/spaceEraser.cpp
@@ -1,4 +1,3 @@
-#include <cctype>
 #include <iostream>
 #include <string>
 
@@ -8,21 +7,11 @@ int main(int argc, char *argv[]) {
   for (int j = 1; j < argc; j++) {
     string word = argv[j];
     int m = 1;
-    int f = isspace(word[0]);
 
-<<<<<<< HEAD
     cout << ' ';
-=======
-    if (f == 0) {
-      cout << ' ';
-    }
->>>>>>> 72eebfa57b20d33dbc47d377774c07ad64ce3ba2
 
     for (int i = 0; i < word.size(); i++) {
-      int x = isspace(word[i]);
-      int z = isspace(word[m]);
-
-      if (x == 0 || z == 0) {
+      if (word[i] != ' ' || word[m] != ' ') {
         cout << word[i];
       }
       m++;
