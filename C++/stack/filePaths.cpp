5d8fa999d7e69e05e89312895acf4eb638c39b2a
faber222 <faberbernardo78@gmail.com>
2022-04-22 09:09:39 -0300

    Learning C++

--- C++/stack/filePaths.cpp
+++ C++/stack/filePaths.cpp
@@ -1,34 +1,48 @@
 #include <cstring>
 #include <iostream>
+#include <queue>
 #include <stack>
 #include <string>
 
 using namespace std;
 
+void separa(const string& algo, string sep, queue<string>& q) {
+  int x = 0;
+  string memoria;
+
+  while (x != string::npos) {
+    int z = algo.find_first_not_of(sep, x);
+    if (z == string::npos) break;
+    x = algo.find(sep, z);
+    memoria = algo.substr(z, x - z);
+    q.push(memoria);
+  }
+};
+
 // reduz o "caminho", retornando como resultado o caminho reduzido
 string reduz_caminho(const string& caminho) {
   stack<string> p1;
   stack<string> p2;
-  int x = 0;
+  queue<string> q1;
   string separador = "/";
+  string z = "";
   string memoria, palavra;
-  if (caminho.size() == 0) {
-    return "/";
+
+  if (caminho == "") {
+    return z;
   }
 
-  while (x != string::npos) {
-    int z = caminho.find_first_not_of(separador, x);
-    if (z == string::npos) break;
-    x = caminho.find(separador, z);
-    memoria = caminho.substr(z, (x - z));
-    if (memoria.size() == 0) {
-      return "/";
-    }
+  separa(caminho, separador, q1);
+  while (!q1.empty()) {
+    memoria = q1.front();
+    q1.pop();
 
     if (memoria != ".." && memoria != ".") {
       p1.push(memoria);
     } else if (memoria == "..") {
-      p1.pop();
+      if (!p1.empty()) {
+        p1.pop();
+      }
     }
   }
 
@@ -41,7 +55,8 @@ string reduz_caminho(const string& caminho) {
     palavra += p2.top();
     p2.pop();
   }
-  return palavra;
+
+  return separador;
 }
 
 int main() {
