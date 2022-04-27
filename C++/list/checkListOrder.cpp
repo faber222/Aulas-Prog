b7d1d7ec50a787f3430772d950e3a6717abadf1e
faber222 <faberbernardo78@gmail.com>
2022-04-27 11:16:21 -0300

    Learning C++

--- C++/list/checkListOrder.cpp
+++ C++/list/checkListOrder.cpp
@@ -7,19 +7,44 @@
 
 using namespace std;
 
-bool ordenada(const list<int>& umaLista){};
+/*
+push_back: Adicionar um dado ao final
+push_front: Adicionar um dado no início
+pop_front: Remover um dado do início
+pop_back: Remover um dado do final
+insert: Inserir um dado em uma determinada posição
+erase: Remover um ou mais dados a partir de uma determinada posição
+front: Acessar o dado que está no início
+back: Acessar o dado que está no final
+size: Obter a quantidade de dados armazenados
+clear: Remover todos os dados (esvaziar)
+declarar lista com lint<parametro> nome;
+*/
+
+bool ordenada(const list<int>& umaLista) {
+  int menor = 0;
+
+  for (auto& compara : umaLista) {
+    if (compara < menor) {
+      return false;
+    } else {
+      menor = compara;
+    }
+  }
+  return true;
+};
 
 int main() {
   srand(time(NULL));
   list<int> lista;
   string arquivo;
-  int linha;
   int vetor[100];
-  int aux;
+  int aux, linha;
 
   for (int i = 0; i < 100; i++) {
     vetor[i] = rand() % 100;
   }
+
   for (int i = 0; i < 100; i++) {
     for (int j = i + 1; j < 100; j++) {
       if (vetor[i] > vetor[j]) {
@@ -42,6 +67,8 @@ int main() {
   while (arq2 >> linha) {  // le linha por linha
     lista.push_back(linha);
   }
+  bool teste = ordenada(lista);
+  cout << teste << endl;
 
   for (auto it = lista.begin(); it != lista.end(); it++) {
     cout << "Numero: " << *it << endl;
