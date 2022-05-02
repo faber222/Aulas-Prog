51ff53529fbfca652f309a59fc712095e3f7910a
faber222 <faberbernardo78@gmail.com>
2022-05-02 00:50:18 -0300

    Learning C++

--- C++/list/outliersList.cpp
+++ C++/list/outliersList.cpp
@@ -7,40 +7,34 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
   ifstream arq(argv[1]);
-  list<int> lista, valores;
+  list<int> lista;
   float media = 0;
-  int soma = 0;
-  int n;
-  int x = 0;
+  float porcentagem = 0;
+  int soma = 0, x = 0, n;
 
   while (arq >> n) {
     lista.push_back(n);
   }
-  for (auto it = lista.begin(); it != lista.end(); it++) {
-    soma = soma + *it;
-    x++;
-  } /*
-   for (auto& i : lista) {
-     soma = soma + i;
-     x++;
-   }*/
-  media = soma / x;
-  // cout << media << endl;
 
-  for (auto t = lista.begin(); t != lista.end(); t++) {
-    if (*t < ((media * 20) / 100)) {
-      valores.push_back(*t);
+  for (auto& i : lista) {
+    soma = soma + i;
+    x++;
+  }
+
+  media = soma / x;
+  porcentagem = 0.2 * media;
+  media = media + porcentagem;
+
+  for (auto z = lista.begin(); z != lista.end();) {
+    if (*z >= media) {
+      z = lista.erase(z);
+    } else {
+      z++;
     }
   }
-  /*
-  for (auto& x : lista) {
-    if (x <= ((media * 20) / 100)) {
-      valores.push_back(x);
-    }
-  }*/
 
-  while (!valores.empty()) {
-    cout << valores.front() << endl;
-    valores.pop_front();
+  while (!lista.empty()) {
+    cout << lista.front() << endl;
+    lista.pop_front();
   }
 }
